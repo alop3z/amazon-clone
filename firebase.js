4537a2880be757ceaa9aeb3beadbc04f8a819e2a
Armando Lopez <mandokp2@gmail.com>
2021-07-05 17:43:05 -0400

    day3a

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+	apiKey: 'AIzaSyDVNHbWmvqm85sqZWYKm2rgpko8yrLpnaA',
+	authDomain: 'amzn-2-bf82b.firebaseapp.com',
+	projectId: 'amzn-2-bf82b',
+	storageBucket: 'amzn-2-bf82b.appspot.com',
+	messagingSenderId: '1082730123927',
+	appId: '1:1082730123927:web:3575b6527a09c458214fd2',
+}
--- src/slices/basketSlice.js
+++ src/slices/basketSlice.js
@@ -1,21 +1,42 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
-  items: [],
-};
+	items: [],
+}
 
 export const basketSlice = createSlice({
-  name: "basket",
-  initialState,
-  reducers: {
-    addToBasket: (state, action) => {},
-    removeFromBasket: (state, action) => {},
-  },
-});
+	name: 'basket',
+	initialState,
+	reducers: {
+		// Actions
+		addToBasket: (state, action) => {
+			state.items = [...state.items, action.payload]
+		},
+		removeFromBasket: (state, action) => {
+			const index = state.items.findIndex(
+				(basketItem) => basketItem.id === action.payload.id
+			)
+			let newBasket = [...state.items]
 
-export const { addToBasket, removeFromBasket } = basketSlice.actions;
+			if (index >= 0) {
+				//item exists in the basket
+				newBasket.splice(index, 1)
+			} else {
+				console.warn(
+					`Cant remove product (id: ${action.payload.id}) as it is not in the cart. `
+				)
+			}
+
+			state.items = newBasket
+		},
+	},
+})
+
+export const { addToBasket, removeFromBasket } = basketSlice.actions
 
 // Selectors - This is how we pull information from the Global store slice
-export const selectItems = (state) => state.basket.items;
+export const selectItems = (state) => state.basket.items
+export const selectTotal = (state) =>
+	state.basket.items.reduce((total, item) => total + item.price, 0)
 
-export default basketSlice.reducer;
+export default basketSlice.reducer
